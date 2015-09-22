a604acaaa2cfd1681bf2a453ee3508a9136ec539
Rik Barwick <rik@creativitea.co.uk>
2015-09-23 00:08:09 +0100

    config

--- Site/perch/config/config.php
+++ Site/perch/config/config.php
@@ -1,10 +1,10 @@
 <?php
     define('PERCH_LICENSE_KEY', 'P21509-XXW087-ULZ976-KBT373-JNT022');
 
-    define("PERCH_DB_USERNAME", 'root');
-    define("PERCH_DB_PASSWORD", 'root');
+    define("PERCH_DB_USERNAME", 'perch_tea');
+    define("PERCH_DB_PASSWORD", 'Te@2sugars');
     define("PERCH_DB_SERVER", "localhost");
-    define("PERCH_DB_DATABASE", "creativitea");
+    define("PERCH_DB_DATABASE", "admin_tea");
     define("PERCH_DB_PREFIX", "perch2_");
     
     define('PERCH_TZ', 'UTC');
@@ -12,7 +12,7 @@
     define('PERCH_EMAIL_FROM', 'rik@creativitea.co.uk');
     define('PERCH_EMAIL_FROM_NAME', 'Rik Barwick');
 
-    define('PERCH_LOGINPATH', '/creativitea/Site/perch');
+    define('PERCH_LOGINPATH', '/perch');
     define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
     define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
 
