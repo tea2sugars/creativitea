34cda1cd24d1800cdd74c4eab34f1528cf643911
Rik Barwick <rik@creativitea.co.uk>
2015-09-23 00:09:41 +0100

    config

--- Site/perch/config/config.php
+++ Site/perch/config/config.php
@@ -1,18 +1,18 @@
 <?php
-    define('PERCH_LICENSE_KEY', 'P21509-XXW087-ULZ976-KBT373-JNT022');
+    define('PERCH_LICENSE_KEY', '$licenseKey');
 
-    define("PERCH_DB_USERNAME", 'perch_tea');
-    define("PERCH_DB_PASSWORD", 'Te@2sugars');
-    define("PERCH_DB_SERVER", "localhost");
-    define("PERCH_DB_DATABASE", "admin_tea");
+    define("PERCH_DB_USERNAME", '$db_username');
+    define("PERCH_DB_PASSWORD", '$db_password');
+    define("PERCH_DB_SERVER", "$db_server");
+    define("PERCH_DB_DATABASE", "$db_database");
     define("PERCH_DB_PREFIX", "perch2_");
     
-    define('PERCH_TZ', 'UTC');
+    define('PERCH_TZ', '$tz');
 
-    define('PERCH_EMAIL_FROM', 'rik@creativitea.co.uk');
-    define('PERCH_EMAIL_FROM_NAME', 'Rik Barwick');
+    define('PERCH_EMAIL_FROM', '$userEmail');
+    define('PERCH_EMAIL_FROM_NAME', '$userGivenName $userFamilyName');
 
-    define('PERCH_LOGINPATH', '/perch');
+    define('PERCH_LOGINPATH', '$loginpath');
     define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
     define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
 
