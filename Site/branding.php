c422591cc1ab0a4cd714dcb77afc4ee853713d1e
Rik Barwick <rik@creativitea.co.uk>
2015-09-23 16:15:58 +0100

    Updated Brand Header Image

--- Site/branding.php
+++ Site/branding.php
@@ -46,7 +46,7 @@
 	<div class="wrapper">
 
 		<!-- HOME -->
-		<section class="module module-header bg-dark bg-dark-50" data-background="assets/images/digital_header.jpg">
+		<section class="module module-header bg-dark bg-dark-50" data-background="assets/images/brand-general_header.jpg">
 
 			<div class="container">
 
