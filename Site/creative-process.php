74c8efdb17bbd2ae43318ab8a485230cc6222879
Rik Barwick <rik@creativitea.co.uk>
2015-09-23 17:20:12 +0100

    fixed columns

--- Site/creative-process.php
+++ Site/creative-process.php
@@ -72,40 +72,40 @@
 				</div>
 				
 				<div class="multi-columns-row">
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						<h3>Brief</h3>
 						<p>We believe that a project's success can be judged on the quality of the design process. It all begins with a brief; whether you have a solid idea in mind or not, it's always good to talk about your vision over a cuppa. Whatever your requirements are, we'll work towards creating a bespoke, well-crafted and engaging solution.</p>
 					</div>
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						image
 					</div>
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						image
 					</div>
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						<h3>Research</h3>
 <p>For us, the research stage is as important as the final design itself. Every project is thoroughly researched; including target audience, competitors, market trends and best use of technology and materials. This ensures we can guide the project in the best possible direction and execute the work to the highest standard. Our research and initial ideas are always put forward to you for feedback before we progress to the concept stage.</p>
 					</div>
 					
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						<h3>Ideas and concepts</h3>
 <p>The studio team works together to envisage and share concepts, fully exploring each idea using wireframes, visuals and prototypes. Working this way helps us to push ideas further, to efficiently and imaginatively create a project solution. These concepts are presented to you to ensure we are headed in the right direction, before we get the green light to proceed with the execution stage.</p>
 					</div>
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						image
 					</div>
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						image
 					</div>
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						<h3>Development</h3>
 <p>With the concept approved, we can start bringing the project to life. Our careful attention to detail ensures the final product is something you'll take pride in. Whether its a print-based or digital project, your evaluation and final sign-off on the project is an important aspect in getting ready for launch.</p>
 					</div>
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						<h3>Delivery</h3>
 <p>We take great pride in our work, making certain that at the conclusion of the project our clients come away happy. Once the project has been launched there is an opportunity for us to gain feedback, make improvements and measure goals. This stage can also be the start of an ongoing creative partnership with tea*.</p>
 					</div>
-					<div class="col-sml-6">
+					<div class="col-sm-6">
 						image
 					</div>
 				</div>
