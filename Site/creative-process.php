9ac9d8cb04e4e17493762919a79a0f9032b242d1
Rik Barwick <rik@creativitea.co.uk>
2015-09-24 11:05:15 +0100

    aligned footer CTA

--- Site/creative-process.php
+++ Site/creative-process.php
@@ -61,7 +61,10 @@
 
 							<article class="post-entry">
 
-								<p class="lead">Creativitea™ is an independently run creative design studio co-founded by Rik Barwick and Lloyd Bedford</p>
+								<p class="lead">You can't force ideas, that's not the game...</p>
+								<p>Creative ideas are like invisible elements in the atmosphere that attract to some people more than others. We're lucky to be able to think differently and explore concepts that may seem a bit "out of the box" which then get pulled together to deliver something tangible for our clients.</p>
+								<p>Rob's favourite way of thinking is doodling, Lloyd's is exploring through his software, Rik's is chatting to his pigs.</p>
+								<p>There is no real formula for idea generation. We have a process which we loosely work towards which is below but every client and project is different so we adapt our working process to suit.</p>
 							</article>
 							
 						</div>
@@ -72,41 +75,47 @@
 				</div>
 				
 				<div class="multi-columns-row">
-					<div class="col-sm-6">
+					<div class="col-sm-6 process-block">
 						<h3>Brief</h3>
 						<p>We believe that a project's success can be judged on the quality of the design process. It all begins with a brief; whether you have a solid idea in mind or not, it's always good to talk about your vision over a cuppa. Whatever your requirements are, we'll work towards creating a bespoke, well-crafted and engaging solution.</p>
 					</div>
-					<div class="col-sm-6">
-						<img src="assets/images/process_brief.gif" alt="process_brief" width="550" height="302">
+					<div class="col-sm-6 process-block">
+						<img src="assets/images/process_brief.gif" alt="process_brief" >
 					</div>
 					<div class="col-sm-6">
-						<img src="assets/images/process_research.gif" alt="process_research" width="550" height="302">
+						<img src="assets/images/process_research.gif" alt="process_research" >
 					</div>
-					<div class="col-sm-6">
+					<div class="col-sm-6 process-block">
 						<h3>Research</h3>
 <p>For us, the research stage is as important as the final design itself. Every project is thoroughly researched; including target audience, competitors, market trends and best use of technology and materials. This ensures we can guide the project in the best possible direction and execute the work to the highest standard. Our research and initial ideas are always put forward to you for feedback before we progress to the concept stage.</p>
 					</div>
 					
-					<div class="col-sm-6">
+					<div class="col-sm-6 process-block">
 						<h3>Ideas and concepts</h3>
 <p>The studio team works together to envisage and share concepts, fully exploring each idea using wireframes, visuals and prototypes. Working this way helps us to push ideas further, to efficiently and imaginatively create a project solution. These concepts are presented to you to ensure we are headed in the right direction, before we get the green light to proceed with the execution stage.</p>
 					</div>
-					<div class="col-sm-6">
-						<img src="assets/images/process_ideas.gif" alt="process_ideas" width="550" height="302">
+					<div class="col-sm-6 process-block">
+						<img src="assets/images/process_ideas.gif" alt="process_ideas" >
 					</div>
-					<div class="col-sm-6">
-						<img src="assets/images/process_development.gif" alt="process_development" width="550" height="302">
+					<div class="col-sm-6 process-block">
+						<img src="assets/images/process_development.gif" alt="process_development" >
 					</div>
-					<div class="col-sm-6">
+					<div class="col-sm-6 process-block">
 						<h3>Development</h3>
 <p>With the concept approved, we can start bringing the project to life. Our careful attention to detail ensures the final product is something you'll take pride in. Whether its a print-based or digital project, your evaluation and final sign-off on the project is an important aspect in getting ready for launch.</p>
 					</div>
-					<div class="col-sm-6">
+					<div class="col-sm-6 process-block">
 						<h3>Delivery</h3>
 <p>We take great pride in our work, making certain that at the conclusion of the project our clients come away happy. Once the project has been launched there is an opportunity for us to gain feedback, make improvements and measure goals. This stage can also be the start of an ongoing creative partnership with tea*.</p>
 					</div>
-					<div class="col-sm-6">
-						<img src="assets/images/process_signoff.gif" alt="process_signoff" width="550" height="302">
+					<div class="col-sm-6 process-block">
+						<img src="assets/images/process_signoff.gif" alt="process_signoff" >
+					</div>
+				</div>
+				<div class="row">
+					<div class="portfolio-contact text-center">
+										<p class="lead">Do you have an idea you want to bring to life? We would love to hear from you.</p>
+										<a href="#" class="btn btn-y btn-round btn-lg">Contact Us</a>
 					</div>
 				</div>
 
