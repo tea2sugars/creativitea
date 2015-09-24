2b3b7a2e77b9db66271ff74f32ef8729d72f6d5e
Rik Barwick <rik@creativitea.co.uk>
2015-09-24 17:28:57 +0100

    changed idea factory to Ideas & Disruption

---
+++ Site/ideas.php
@@ -0,0 +1,551 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<title>Creativitea - A brand, digital and print design agency | London, Guildford, Suffolk</title>
+	
+	<!-- Bootstrap core CSS -->
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- Plugins -->
+	<link href="assets/css/simpletextrotator.css" rel="stylesheet">
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+	<link href="assets/css/et-line-font.css" rel="stylesheet">
+	<link href="assets/css/magnific-popup.css" rel="stylesheet">
+	<link href="assets/css/flexslider.css" rel="stylesheet">
+	<link href="assets/css/animate.css" rel="stylesheet">
+	
+	<!-- Template core CSS -->
+	<link href="assets/css/style.css" rel="stylesheet">
+	
+	<!-- Custom css -->
+	<link href="assets/css/custom.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6440332/629266/css/fonts.css" />
+	
+	<!--Share widget-->
+	<script type="text/javascript">var switchTo5x=true;</script>
+	<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+	<script type="text/javascript">stLight.options({publisher: "0750579a-dfe6-4d2b-a96d-da69d0aa188f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+	
+</head>
+<body>
+<!-- PRELOADER -->
+	<div class="page-loader">
+		<div class="loader">Brewing...</div>
+	</div>
+	<!-- /PRELOADER -->
+
+	<?php include('assets/php/includes/nav-transparent.html');?>
+
+
+	<!-- WRAPPER -->
+	<div class="wrapper">
+
+		<!-- HOME -->
+		<section class="module module-header bg-dark bg-dark-50" data-background="assets/images/digital_header.jpg">
+
+			<div class="container">
+
+				<!-- MODULE TITLE -->
+				<div class="row">
+
+					<div class="col-sm-6 col-sm-offset-3">
+
+						<h1 class="module-title align-center">Ideas and Disruption</h1>
+
+						<div class="module-subtitle align-center">
+							<p class="lead">Working with you to create ideas, concepts and to stir the pot</p>
+							<a class="btn btn-lg btn-y btn-round" href="contact">Get in touch to discuss your project</a>
+
+						</div>
+					</div>
+
+				</div>
+				<!-- /MODULE TITLE -->
+
+			</div>
+
+		</section >
+		<!-- /HOME -->
+
+		<section class="module">
+
+			<div class="container">
+				
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2">
+						
+
+							<article>
+
+								<p class="lead">We're a creative digital agency founded on creative thinking, technical knowhow and a thirst for customer satisfaction, over-delivering on our clients KPI's and expectations.</p>
+<p>A great idea is nothing without execution, and we have the people and experience to make things happen. We have more than a decade of experience delivering highly-effective digital communications for some of the biggest brands in the country, we use this expertise to scale ideas for businesses and charities of all shapes and sizes – Winning awards as we go. </p>
+<p>Creativitea has developed a team of trusted, experienced digital folk that adapt existing services, innovate and develop digital projects that cater for any budget. With creative thinking there's always a way!</p> 
+							</article>
+							<article>
+								<p>At Creativitea, we collaboratively with our clients to deliver creative digital solutions that deliver results. We take time to understand your business aspirations and the culture surrounding your brand in order to partner with you at any point in your digital journey. From a designing a website to integrated campaigns on social media, mobile and advertising. 
+									
+								</p>
+							</article>
+							<!--<article>
+								 
+									<div class="col-sm-8">
+				
+										<h5 class="progress-title font-inc">Your Digital Communications</h5>
+										<div class="progress">
+											<div class="progress-bar pb-dark" aria-valuenow="100" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+												<span class="font-inc"></span>
+											</div>
+										</div>
+				
+										<h5 class="progress-title font-inc">Responsive Website Design </h5>
+										<div class="progress">
+											<div class="progress-bar pb-dark" aria-valuenow="80" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+												<span class="font-inc"></span>
+											</div>
+										</div>
+				
+										<h5 class="progress-title font-inc">SEO and Online PR</h5>
+										<div class="progress">
+											<div class="progress-bar pb-dark" aria-valuenow="40" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+												<span class="font-inc"></span>
+											</div>
+										</div>
+										
+										<h5 class="progress-title font-inc">Social Marketing</h5>
+										<div class="progress">
+											<div class="progress-bar pb-dark" aria-valuenow="40" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+												<span class="font-inc"></span>
+											</div>
+										</div>
+				
+										<h5 class="progress-title font-inc">Digital Marketing</h5>
+										<div class="progress">
+											<div class="progress-bar pb-dark" aria-valuenow="40" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+												<span class="font-inc"></span>
+											</div>
+										</div>
+										
+										<h5 class="progress-title font-inc">Content Planning</h5>
+										<div class="progress">
+											<div class="progress-bar pb-dark" aria-valuenow="40" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+												<span class="font-inc"></span>
+											</div>
+										</div>
+				
+									</div>
+									 
+							</article>-->
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2">
+							<article>
+								<p>It's never been more important to engage with your audience via your digital channels. Get in touch if you have something you want to have a chat about regarding your digital world.</p>
+							</article>
+							<a href="portfolio" class="btn btn-g btn-round">View work</a> <a href="contact" class="btn btn-d btn-round">Contact Us</a>
+					</div>
+							
+						<!-- /POST -->
+
+					</div>
+				</div>
+			</div>
+		</section>
+		
+				<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+		
+		<!-- PORTFOLIO -->
+		<section class="module-small p-t-20 p-b-40 p-t-sm-0" id="portfolio">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="align-center">Some of our Digital Projects</h2>
+				</div>
+				<div class="row">
+					<!-- PORTFOLIO LIST -->
+					<ul id="works-grid" class="works-grid works-grid-3 works-hover-w">
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-5.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-1.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">CrawfordJames Website Design</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Responsive Website Design<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-4.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-3.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-2.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+					
+		
+					</ul>
+				</div>
+			</div>
+			
+		</section>
+		<!-- /PORTFOLIO -->
+		
+		<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+		
+		
+		<section class="module-small" id="services">
+
+			<div class="container">
+				<div class="row">
+					<!-- CONTENT BOX -->
+					<div class="col-sm-6 col-md-3">
+						<div class="content-box">
+							<div class="content-box-icon">
+								<span class="icon-beaker yellow"></span>
+							</div>
+							<div class="content-box-title table-display">
+								<div class="table-cell">
+									<h4><a href="#">Branding</a></h4>
+								</div>
+							</div>
+							<div class="content-box-text">
+								<p>Cred listicle XOXO, Truffaut post-ironic PBR trust fund mlkshk shabby chic art party. Small batch disrupt bicycle rights, artisan cray Odd Future Tumblr</p>
+							</div>
+							<div class="content-box-text">
+								<span class="yellow lead">–</span>
+								<ul>
+									<li><a href="#">Digital Design</a></li>
+									<li><a href="#">UX Design</a></li>
+									<li><a href="#">Website Design & Development</a></li>
+									<li><a href="#">Digital Marketing</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /CONTENT BOX -->
+					<!-- CONTENT BOX -->
+					<div class="col-sm-6 col-md-3">
+						<div class="content-box">
+							<div class="content-box-icon">
+								<span class="icon-mobile yellow"></span>
+							</div>
+							<div class="content-box-title table-display">
+								<div class="table-cell">
+									<h4><a href="#">Digital & Website Design</a></h4>
+								</div>
+							</div>
+							<div class="content-box-text">
+								<p>Cred listicle XOXO, Truffaut post-ironic PBR trust fund mlkshk shabby chic art party. Small batch disrupt bicycle rights, artisan cray Odd Future Tumblr</p>
+							</div>
+							<div class="content-box-text">
+								<span class="yellow lead">–</span>
+								<ul>
+									<li><a href="#">Digital Design</a></li>
+									<li><a href="#">UX Design</a></li>
+									<li><a href="#">Website Design & Development</a></li>
+									<li><a href="#">Digital Marketing</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /CONTENT BOX -->
+					<!-- CONTENT BOX -->
+					<div class="col-sm-6 col-md-3">
+						<div class="content-box">
+							<div class="content-box-icon">
+								<span class="icon-printer yellow"></span>
+							</div>
+							<div class="content-box-title table-display">
+								<div class="table-cell">
+									<h4><a href="#">Print Design</a></h4>
+								</div>
+							</div>
+							<div class="content-box-text">
+								<p>Cred listicle XOXO, Truffaut post-ironic PBR trust fund mlkshk shabby chic art party. Small batch disrupt bicycle rights, artisan cray Odd Future Tumblr</p>
+							</div>
+							<div class="content-box-text">
+								<span class="yellow lead">–</span>
+								<ul>
+									<li><a href="#">Digital Design</a></li>
+									<li><a href="#">UX Design</a></li>
+									<li><a href="#">Website Design & Development</a></li>
+									<li><a href="#">Digital Marketing</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /CONTENT BOX -->
+					<!-- CONTENT BOX -->
+					<div class="col-sm-6 col-md-3">
+						<div class="content-box">
+							<div class="content-box-icon">
+								<span class="icon-video yellow"></span>
+							</div>
+							<div class="content-box-title table-display">
+								<div class="table-cell">
+									<h4><a href="#">Motion Design & Animation</a></h4>
+								</div>
+							</div>
+							<div class="content-box-text">
+								<p>Cred listicle XOXO, Truffaut post-ironic PBR trust fund mlkshk shabby chic art party. Small batch disrupt bicycle rights, artisan cray Odd Future Tumblr</p>
+							</div>
+							<div class="content-box-text">
+								<span class="yellow lead">–</span>
+								<ul>
+									<li><a href="#">Digital Design</a></li>
+									<li><a href="#">UX Design</a></li>
+									<li><a href="#">Website Design & Development</a></li>
+									<li><a href="#">Digital Marketing</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /CONTENT BOX -->
+				</div>
+				
+				
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-3 m-t-40 m-t-20">
+					<p class="lead text-center">
+						Need some help but not sure what you need?<br/><span class="small">Contact us below for a chat and see what we can do to help <em>– we like a challenge!</em></span>
+					</p>
+					<div class="text-center">
+						<a href="contact.html" class="btn btn-d btn-round btn-lg">Contact Us</a>
+					</div>
+					
+					</div>
+				</div>
+				
+			</div>
+
+		</section>
+		<!-- /CERVICES -->
+
+		<!-- CONTACT -->
+		<section class="module-small dark-section-5">
+		
+			<div class="container">
+		
+				<div class="row">
+		
+					<!-- CONTENT BOX -->
+					<div class="col-sm-3">
+						<div class="content-box">
+							
+							<div class="content-box-title">
+								<h5>Guildford Studio</h5>
+							</div>
+							<div class="content-box-text">
+								<address>
+									Milestone House, Millbrook, Guildford, Surrey, GU1 3YA
+								</address>
+								<p>
+									01483 453 067
+								</p>
+							</div>
+						</div>
+					</div>
+					<!-- /CONTENT BOX -->
+		
+					<!-- CONTENT BOX -->
+					<div class="col-sm-3">
+						<div class="content-box">
+							
+							<div class="content-box-title">
+								<h5>London Office</h5>
+							</div>
+							<div class="content-box-text">
+								<address>
+									Hoxton Road, London
+								</address>
+								<p>
+									0208 123 0246
+								</p>
+							</div>
+						</div>
+					</div>
+					<!-- /CONTENT BOX -->
+		
+					<!-- CONTENT BOX -->
+					<div class="col-sm-3">
+						<div class="content-box">
+							
+							<div class="content-box-title">
+								<h5>Say hello by email</h5>
+							</div>
+							<div class="content-box-text">
+								<p>
+									<a href="mailto:info@creativitea.co.uk">info@creativitea.co.uk</a>
+								</p>
+							</div>
+						</div>
+					</div>
+					<!-- /CONTENT BOX -->
+					<!-- CONTENT BOX -->
+					<div class="col-sm-3">
+						<div class="content-box">
+							
+							<div class="content-box-title">
+								<h5>Be social</h5>
+							</div>
+							<div class="content-box-text">
+								<p class="lead"><a href="#"><i class="fa-twitter fa-icons fa" aria-label="Twitter"></i></a> <a href="#"><i class="fa-facebook fa-icons fa" aria-label="Facebook"></i></a> <a href="#"><i class="fa-instagram fa-icons fa" aria-label="Instagram"></i></a> <a href="#"><i class="fa-behance fa-icons fa" aria-label="Behance"></i></a> <a href="#"><i class="fa-google-plus fa-icons fa" aria-label="Google Plus"></i></a> <a href="#"><i class="fa-youtube fa-icons fa" aria-label="YouTube"></i></a> <a href="#"><i class="fa-vimeo-square fa-icons fa" aria-label="Vimeo"></i></a> <a href="#"><i class="fa-linkedin fa-icons fa" aria-label="LinkedIn"></i></a></p>
+							</div>
+						</div>
+					</div>
+					<!-- /CONTENT BOX -->
+		
+				</div>
+		
+			</div>
+		
+		</section>
+		<!-- /CONTACT -->
+
+		<!-- FOOTER -->
+		<footer class="footer text-uppercase">
+		
+			<div class="container">
+		
+				<div class="row">
+		
+					<div class="col-sm-12 text-center ">
+						<p class="copyright m-b-0">© 2015 <a href="index.html">Creativitea&trade; Limited</a>, All Rights Reserved. | <a href="#">Privacy Statement</a></p>
+					</div>
+		
+				</div>
+		
+			</div>
+		
+		</footer>
+		<!-- /FOOTER -->
+
+	</div>
+	<!-- /WRAPPER -->
+
+	<!-- Scroll-up -->
+	<div class="scroll-up">
+		<a href="#totop"><i class="fa fa-angle-double-up"></i></a>
+	</div>
+	
+	<!-- Javascript files -->
+	<script src="assets/js/jquery-2.1.3.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
+	<script src="assets/js/appear.js"></script>
+	<script src="assets/js/jquery.simple-text-rotator.min.js"></script>
+	<script src="assets/js/jqBootstrapValidation.js"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="assets/js/gmaps.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<script src="assets/js/imagesloaded.pkgd.js"></script>
+	<script src="assets/js/jquery.flexslider-min.js"></script>
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/jquery.fitvids.js"></script>
+	<script src="assets/js/smoothscroll.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/contact.js"></script>
+	<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
