1b16f16486f413dfa4ecd2770d72afe31fbda589
Rik Barwick <rik@creativitea.co.uk>
2015-09-24 11:21:00 +0100

    video page

--- Site/video-production.php
+++ Site/video-production.php
@@ -93,61 +93,13 @@
 
 <p>So if you need a corporate video or presentation, showreel for an exhibition space, ad for social media or broadcast television, we work with individuals and businesses of all sizes to help bring there a stories to life on screen.</p>
 							</article>
-							<article>
-								<!-- SKILLS -->
-									<div class="col-sm-8">
-				
-										<h5 class="progress-title font-inc">Your Digital Communications</h5>
-										<div class="progress">
-											<div class="progress-bar pb-dark" aria-valuenow="100" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-												<span class="font-inc"></span>
-											</div>
-										</div>
-				
-										<h5 class="progress-title font-inc">Responsive Website Design </h5>
-										<div class="progress">
-											<div class="progress-bar pb-dark" aria-valuenow="80" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-												<span class="font-inc"></span>
-											</div>
-										</div>
-				
-										<h5 class="progress-title font-inc">SEO and Online PR</h5>
-										<div class="progress">
-											<div class="progress-bar pb-dark" aria-valuenow="40" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-												<span class="font-inc"></span>
-											</div>
-										</div>
-										
-										<h5 class="progress-title font-inc">Social Marketing</h5>
-										<div class="progress">
-											<div class="progress-bar pb-dark" aria-valuenow="40" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-												<span class="font-inc"></span>
-											</div>
-										</div>
-				
-										<h5 class="progress-title font-inc">Digital Marketing</h5>
-										<div class="progress">
-											<div class="progress-bar pb-dark" aria-valuenow="40" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-												<span class="font-inc"></span>
-											</div>
-										</div>
-										
-										<h5 class="progress-title font-inc">Content Planning</h5>
-										<div class="progress">
-											<div class="progress-bar pb-dark" aria-valuenow="40" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-												<span class="font-inc"></span>
-											</div>
-										</div>
-				
-									</div>
-									<!-- /SKILLS -->
-							</article>
+							
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-sm-8 col-sm-offset-2">
 							<article>
-								<p>So there's a fair bit to do and this is where we come in. Have a look at some of the other digital design services we provide below.</p>
+								<p>Be sure to check out our video work, get the popcorn in!</p>
 							</article>
 							<a href="portfolio" class="btn btn-g btn-round">View work</a> <a href="contact" class="btn btn-d btn-round">Contact Us</a>
 					</div>
@@ -159,7 +111,138 @@
 			</div>
 		</section>
 		
-				<!-- DIVIDER -->
+								<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+		
+		<!-- PORTFOLIO -->
+		<section class="module-small p-t-20 p-b-40 p-t-sm-0" id="portfolio">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="align-center">Some of our Video & Animation Projects</h2>
+				</div>
+				<div class="row">
+					<!-- PORTFOLIO LIST -->
+					<ul id="works-grid" class="works-grid works-grid-3 works-hover-w">
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-5.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-1.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">CrawfordJames Website Design</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Responsive Website Design<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-4.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-3.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+		
+						<!-- PORTFOLIO ITEM -->
+						<li class="work-item marketing photography">
+							<a href="portfolio-single">
+								<div class="work-image">
+									<img src="assets/images/work-sample-2.jpg" alt="">
+								</div>
+								<div class="work-caption">
+									<h3 class="work-title ">Bag MockUp</h3>
+									<div class="work-descr ">
+										<span class="yellow">–</span><br/>
+										Marketing<br/>
+										<button class="btn btn-y btn-round m-t-20">View project</button>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+					
+		
+					</ul>
+				</div>
+			</div>
+			
+		</section>
+		<!-- /PORTFOLIO -->
+		
+		<!-- DIVIDER -->
 		<hr class="divider-w">
 		<!-- /DIVIDER -->
 		
