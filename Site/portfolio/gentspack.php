95db587be010a07ce8d6e4df6085742508eb2c64
Rik Barwick <rik@creativitea.co.uk>
2015-09-25 16:55:04 +0100

    Gents Pack Project

--- Site/portfolio/gentspack.php
+++ Site/portfolio/gentspack.php
@@ -6,7 +6,7 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 	
-	<title>Creative Portfolio | Creativitea - A brand, digital and print design agency | London, Guildford, Suffolk</title>
+	<title>Gents Pack Re-Brand and Packaging | Creativitea - A brand design agency | London, Guildford, Suffolk</title>
 	
 	<!-- Bootstrap core CSS -->
 	<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -36,11 +36,8 @@
 	<?php include('../assets/php/includes/nav-internal.html');?>
 
 
-	<!-- WRAPPER -->
-	<div class="wrapper">
-
-		<!-- HOME -->
-		<section class="module module-header bg-dark bg-dark-50" data-background="../assets/images/portfolio-header.jpg">
+	<!-- HOME -->
+	<section class="module module-header bg-dark bg-dark-70" data-background="../assets/images/projects/Gents_general_header.jpg">
 
 			<div class="container">
 
@@ -49,14 +46,9 @@
 
 					<div class="col-sm-6 col-sm-offset-3">
 
-						<h1 class="module-title align-center">Our Portfolio of Work</h1>
+						<h1 class="module-title align-center">Gents Pack Branding and Packaging<br/>
+							<span class="small">Giving customers a more complete package</span></h1>
 
-						<div class="module-subtitle align-center">
-							<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-							<div class="portfolio-contact">
-								<a href="#" class="btn btn-y btn-round btn-lg">Contact Us</a>
-							</div>
-						</div>
 
 					</div>
 
@@ -66,241 +58,122 @@
 			</div>
 
 		</section >
-		<!-- /HOME -->
+	<!-- /HOME -->
 
-		<!-- PORTFOLIO -->
-		<section class="module-small p-t-20 p-b-60">
+	<!-- WRAPPER -->
+	<div class="wrapper">
+
+		<!-- PORTFOLIO DESCRIPTION -->
+		<section class="module-small">
+
+			<div class="container">
+				
+				<!--<div class="row">
+					<div class="col-sm-12">
+
+						<p><img src="../assets/images/section-1.jpg" alt=""></p>
+					</div>
+				</div>-->
+				
+				<!-- PORTFOLIO KEY IMAGE -->
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-4">
+
+						<p><img src="../assets/images/projects/Gents_portfolio_01.jpg" alt="Gents Pack Branding Logotype"></p>
+					</div>
+
+				<!-- PORTFOLIO DESCRIPTION -->
+				<div class="row m-t-70">
+
+					<div class="col-md-4">
+						<div class="work-details">
+							<ul>
+								<li><strong>Client: </strong>Gents Pack</li>
+								<li><strong>Project: </strong>Branding, Visual Identity, Packaging</li>
+								<!--<li><strong>Online: </strong><a href="#" target="_blank">www.example.com</a></li>-->
+								<li><strong>Budget: </strong>£££££</li>
+								<!--<li><strong>Testimonial: </strong>
+									<br/>
+									<blockquote>Being a new company, FiveThree had lots of things to worry about - the creation and development of a brand identity wasn't one of them thanks to the creative thinking at Creativitea.</blockquote>
+									Michael Hockey <span class="yellow">|</span> Director
+								</li>-->
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-8">
+
+						<div class="row multi-columns-row">
+
+							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
+								<h5 class="m-t-0">History</h5>
+								<p>Mens Package, a luxury mens club for designer underwear, grooming and accessories, needed a rebrand a head of there re-launch earlier this year.</p>
+							</div>
+
+							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
+								<h5 class="m-t-0">Our Brief</h5>
+								<p>We were approached to re-design the identity under the new name The Gents Pack, and to make the identity have a sense of belong to a exclusive club.</p>
+							</div>
+														
+							<div class="col-sm-12 col-md-12 col-lg-12 m-b-30">
+								<h5 class="m-t-0">What we did</h5>
+								<p>The new identity features a contemporary logo, colour palette, typography system and visual DNA.</p>
+<p>Based around the idea of “the club to join”, we created a identity that reflects the luxury products and lifestyle The Gents Pack provides, whilst giving it a distinctly modern and contemporary feel. The new identity will be visible across marketing materials including advertising, packaging and digital communications.</p>
+<p>Deliverables for the project were a set of guidelines, new full logo design and packaging.</p>
+
+								<div class="portfolio-contact">
+									<p class="lead">Do you have a similar project? We would love to hear from you.</p>
+									<a href="#" class="btn btn-y btn-round btn-lg">Contact Us</a>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+				<!-- /PORTFOLIO DESCRIPTION -->			</div>
+
+		</section>
+		<!-- /PORTFOLIO DESCRIPTION -->
+
+		<!-- PORTFOLIO CONTENT -->
+		<section class="module p-t-0 p-b-0">
 
 			<div class="container">
 
-				<!-- PORTFOLIO FILTER -->
 				<div class="row">
-					<div class="col-sm-12">
 
-						<ul id="filters" class="filter  hidden-xs">
-							<li><a href="#" data-filter="*" class="current wow fadeInUp">All</a></li>
-							<li><a href="#" data-filter=".branding" class="wow fadeInUp" data-wow-delay="0.2s">Branding</a></li>
-							<li><a href="#" data-filter=".digital" class="wow fadeInUp" data-wow-delay="0.4s">Digital Design</a></li>
-							<li><a href="#" data-filter=".webdesign" class="wow fadeInUp" data-wow-delay="0.6s">Web Design</a></li>
-							<li><a href="#" data-filter=".print" class="wow fadeInUp" data-wow-delay="0.8s">Print Design</a></li>
-							<li><a href="#" data-filter=".film" class="wow fadeInUp" data-wow-delay="0.8s">Film & Animation</a></li>
-							<li><a href="#" data-filter=".disrupt" class="wow fadeInUp" data-wow-delay="0.8s">Ideas & Disruption</a></li>
-						</ul>
+					<!--<div class="col-sm-12">-->
+						<div class="col-sm-8 col-sm-offset-4">
+
+						<p><img src="../assets/images/projects/Gents_portfolio_02.jpg" alt="Gents Pack Brand Marque"></p>
+						<p><img src="../assets/images/projects/Gents_portfolio_03.jpg" alt="Gents Pack Stack Logo"></p>
+						<p><img src="../assets/images/projects/Gents_portfolio_04.jpg" alt="Gents Pack Club Badge"></p>
+						<p><img src="../assets/images/projects/Gents_portfolio_05.jpg" alt="Gents Pack Brand Guidelines"></p>
+						<p><img src="../assets/images/projects/Gents_portfolio_06.jpg" alt="Gents Pack Brand Guidelines"></p>
+						<p><img src="../assets/images/projects/Gents_portfolio_07.jpg" alt="Gents Pack Business Cards"></p>
+						<p><img src="../assets/images/projects/Gents_portfolio_08.jpg" alt="Gents Pack Business Cards Details"></p>
+						<p><img src="../assets/images/projects/Gents_portfolio_09.jpg" alt="Gents Pack Packaging"></p>
+
 
 					</div>
-				</div>
-				<!-- /PORTFOLIO FILTER -->
+
+				</div><!-- .row -->
 
 			</div>
 
-			<!-- PORTFOLIO LIST -->
-			<ul id="works-grid" class="works-grid works-grid-4 works-hover-w">
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item branding webdesign print">
-					<a href="fivethree">
-						<div class="work-image">
-							<img src="../assets/images/projects/53_portfolio_thumbnail.jpg" alt="FiveThree Branding">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">FiveThree Branding</h3>
-							<div class="work-descr ">
-								Branding a new Marketing Consultancy
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item branding webdesign print disrupt">
-					<a href="powwownow">
-						<div class="work-image">
-							<img src="../assets/images/projects/Pow_portfolio_thumbnail.jpg" alt="Powwownow Branding">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Powwownow Branding</h3>
-							<div class="work-descr ">
-								Disruptive thinking for better brand communication
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item illustration photography">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-3.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Disk Cover</h3>
-							<div class="work-descr ">
-								Illustration
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item marketing photography">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-4.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Business Card</h3>
-							<div class="work-descr ">
-								Photography
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item print branding">
-					<a href="gentspack">
-						<div class="work-image">
-							<img src="../assets/images/projects/Gents_portfolio_thumbnail.jpg" alt="Gents Pack Rebrand and Packaging">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Gents Pack</h3>
-							<div class="work-descr ">
-								Brand design and Packaging
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item marketing webdesign">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-6.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Business Cards in paper clip</h3>
-							<div class="work-descr ">
-								Marketing
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item marketing webdesign">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-7.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Business Cards in paper clip</h3>
-							<div class="work-descr ">
-								Marketing
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item marketing webdesign">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-1.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Business Cards in paper clip</h3>
-							<div class="work-descr ">
-								Marketing
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item marketing webdesign">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-2.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Business Cards in paper clip</h3>
-							<div class="work-descr ">
-								Marketing
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item marketing webdesign">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-2.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Business Cards in paper clip</h3>
-							<div class="work-descr ">
-								Marketing
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item marketing webdesign">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-2.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Business Cards in paper clip</h3>
-							<div class="work-descr ">
-								Marketing
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-<!-- PORTFOLIO ITEM -->
-				<li class="work-item marketing webdesign">
-					<a href="../ portfolio-single">
-						<div class="work-image">
-							<img src="assets/images/work-2.jpg" alt="">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Business Cards in paper clip</h3>
-							<div class="work-descr ">
-								Marketing
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-						
-
-			</ul>
-			<!-- /PORTFOLIO LIST -->
-
 		</section>
-		<!-- /PORTFOLIO -->
+		<!-- /PORTFOLIO CONTENT -->
 
 		<!-- PAGINATION -->
-		<section class="module-small p-t-0">
+		<section class="module-small p-t-50">
 
 			<div class="container">
 			
-				<div class="pagination  text-uppercase">
+				<div class="pagination font-inc text-uppercase">
 			
-					<a href="#"><i class="fa fa-angle-left"></i> Prev</a>
-					<a href="#">Next <i class="fa fa-angle-right"></i></a>
+					<a href="#" role="button" class="btn btn-lg btn-y btn-round">View portfolio</a>
 			
 				</div>
 			
@@ -430,7 +303,7 @@
 						Need some help but not sure what you need?<br/><span class="small">Contact us below for a chat and see what we can do to help <em>– we like a challenge!</em></span>
 					</p>
 					<div class="text-center">
-						<a href="../contact" class="btn btn-d btn-round btn-lg">Contact Us</a>
+						<a href="contact.html" class="btn btn-d btn-round btn-lg">Contact Us</a>
 					</div>
 					
 					</div>
@@ -495,7 +368,7 @@
 							</div>
 							<div class="content-box-text">
 								<p>
-									<a href="../mailto:info@creativitea.co.uk">info@creativitea.co.uk</a>
+									<a href="mailto:info@creativitea.co.uk">info@creativitea.co.uk</a>
 								</p>
 							</div>
 						</div>
@@ -530,7 +403,7 @@
 				<div class="row">
 		
 					<div class="col-sm-12 text-center ">
-						<p class="copyright m-b-0">© 2015 <a href="../index">Creativitea&trade; Limited</a>, All Rights Reserved. | <a href="#">Privacy Statement</a></p>
+						<p class="copyright m-b-0">© 2015 <a href="index.html">Creativitea&trade; Limited</a>, All Rights Reserved. | <a href="#">Privacy Statement</a></p>
 					</div>
 		
 				</div>
@@ -555,7 +428,7 @@
 	<script src="../assets/js/appear.js"></script>
 	<script src="../assets/js/jquery.simple-text-rotator.min.js"></script>
 	<script src="../assets/js/jqBootstrapValidation.js"></script>
-	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="../http://maps.google.com/maps/api/js?sensor=true"></script>
 	<script src="../assets/js/gmaps.js"></script>
 	<script src="../assets/js/isotope.pkgd.min.js"></script>
 	<script src="../assets/js/imagesloaded.pkgd.js"></script>
