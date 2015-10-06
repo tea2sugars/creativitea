66badf6295762c6416e93b6d32ee694f319766a5
Rik Barwick <rik@creativitea.co.uk>
2015-10-06 19:53:34 +0100

    removed a load of blocking css scripts and created a compressed theme file to hold all the code

--- Site/index.php
+++ Site/index.php
@@ -12,12 +12,8 @@
 	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	
 	<!-- Plugins -->
-	<link href="assets/css/simpletextrotator.css" rel="stylesheet">
+	<link href="assets/css/theme.css" rel="stylesheet">
 	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
-	<link href="assets/css/et-line-font.css" rel="stylesheet">
-	<link href="assets/css/magnific-popup.css" rel="stylesheet">
-	<link href="assets/css/flexslider.css" rel="stylesheet">
-	<link href="assets/css/animate.css" rel="stylesheet">
 	
 	<!-- Template core CSS -->
 	<link href="assets/css/style.css" rel="stylesheet">
