bf11613b5176f2edf2b724149baf9b48a282b38d
Rik Barwick <rik@creativitea.co.uk>
2015-10-08 16:10:18 +0100

    5mm portfolio update

--- Site/portfolio/5mm.php
+++ Site/portfolio/5mm.php
@@ -25,7 +25,7 @@
 
 					<div class="col-sm-6 col-sm-offset-3">
 
-						<h1 class="module-title align-center">Powwownow Branding<br/><span class="small">Disruptive thinking for better brand communication</span></h1>
+						<h1 class="module-title align-center">5mm Interior Design Responsive Website design<br/><span class="small">Using the browser as a tool to deliver stunning imagery</span></h1>
 
 
 					</div>
@@ -57,7 +57,7 @@
 				<div class="row">
 					<div class="col-sm-8 col-sm-offset-4">
 
-						<p><img src="../assets/images/projects/5mm_portfolio_01.jpg" alt="5mm Responsive Website Design"></p>
+						<p><img src="../assets/images/projects/5mm_portfolio_03.jpg" alt="5mm Responsive Website Design"></p>
 					</div>
 
 				<!-- PORTFOLIO DESCRIPTION -->
@@ -69,7 +69,7 @@
 								<li><strong>Client: </strong>5mm</li>
 								<li><strong>Project: </strong>Responsive Website Design & Development, Bootstrap, Perch CMS</li>
 								<li><strong>Online: </strong><a href="http:www.5mm.co" target="_blank">www.5mm.co</a></li>
-								<li><strong>Budget: </strong>£££££</li>
+								<li><strong>Budget: </strong>££££</li>
 								<!--<li><strong>Testimonial: </strong>
 									<br/>
 									<blockquote>Being a new company, FiveThree had lots of things to worry about - the creation and development of a brand identity wasn't one of them thanks to the creative thinking at Creativitea.</blockquote>
@@ -85,20 +85,19 @@
 
 							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
 								<h5 class="m-t-0">History</h5>
-								<p>Putting customers first, investing in innovation and keeping things simple, Powwownow operates in 15 countries and has been one of the leading providers of voice and video conference call services to business of all shapes and sizes.
-</p>
+								<p>5mm is a London-based but globally minded high end residential interiors design consultancy</p>
 							</div>
 
 							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
 								<h5 class="m-t-0">Our Brief</h5>
-								<p>Sometimes a business needs to look outside of its own walls to find the answers they need to take it further, getting the thoughts and ideas of others not associated with the company. We call this ‘ideas and disruption’.With there growing portfolio of voice and video conferencing services as well business growth, Powwownow approached us to help them review their current brand identity,  with the view of working together to explore new direction and help define their new ‘smarter working’ approach.</p>
+								<p>Our brief was to re-design 5mm's website to help them show off key projects and their fabulous photography.</p>
 							</div>
 														
 							<div class="col-sm-12 col-md-12 col-lg-12 m-b-30">
 								<h5 class="m-t-0">What we did</h5>
-								<p>With the use of regular workshops as a collaborative team, we reviewed the existing strategy, identity and company touch points, asking questions as Why and challenging to gain further and more in-depth thought about Powwownow.</p>
-								<p>A collective decision was made that a new, better defined proposition - company vision was need to align it more with the direction the business was taking and their strategic planning. We devised a series of new ‘visions’ based on the finding from the workshops, deciding on the new statement of ‘When it matters’, underpinned by there quality of service, reliability, customer care and reputation.</p>
-								<p>This translated into the development a of proposed brand redesign, including new logo, visual identity and tone of voice. </p>
+								<p>We were under quite a tight budget and deadline for this project so we stepped up to the challenge and opted to build 5mm a website using Twitter Bootstrap which enable us to efficiently build and design a elegant website around the framework.</p>
+								<p>We opted to use full-screen, scalable imagery for the clients photography to really help show off their work in the browser. The website is a means of serving this content and we didn't want the website to draw attention away from our clients amazing portfolio.</p>
+								<p>The website scales gracefully across different devices ensuring the content looks beautiful wherever their users happen to be.
 
 								<div class="portfolio-contact">
 									<p class="lead">Do you have a similar project? We would love to hear from you.</p>
@@ -126,9 +125,9 @@
 					<!--<div class="col-sm-12">-->
 						<div class="col-sm-8 col-sm-offset-4">
 
-						<p><img src="../assets/images/projects/5mm_portfolio_02.jpg" alt="5mm Mobile Website"></p>
-						<p><img src="../assets/images/projects/5mm_portfolio_03.jpg" alt="5mm Desktop Website"></p>
-						<p><img src="../assets/images/projects/5mm_portfolio_05.jpg" alt="5mm Portfolio Website"></p>
+						<p><img src="../assets/images/projects/5mm_portfolio_01.jpg" alt="5mm Mobile Website"></p>
+						<p><img src="../assets/images/projects/5mm_portfolio_05.jpg" alt="5mm Desktop Website"></p>
+						<p><img src="../assets/images/projects/5mm_portfolio_02.jpg" alt="5mm Portfolio Website"></p>
 						</div>
 
 				</div><!-- .row -->
