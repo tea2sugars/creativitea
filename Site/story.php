5fda8cc4a28699e4bbd785325c6c3f96d59bea24
Rik Barwick <rik@creativitea.co.uk>
2015-10-12 11:05:53 +0100

    formatting

--- Site/story.php
+++ Site/story.php
@@ -18,11 +18,6 @@
 	
 </head>
 <body>
-<!-- PRELOADER -->
-	<div class="page-loader">
-		<div class="loader">Brewing...</div>
-	</div>
-	<!-- /PRELOADER -->
 
 	<?php include('assets/php/includes/nav-fixed.html');?>
 
@@ -84,25 +79,7 @@
 
 		<?php include('assets/php/includes/footer-services.html');?>
 		<?php include('assets/php/includes/footer-contact.html');?>
-	
-	<!-- Javascript files -->
-	<script src="assets/js/jquery-2.1.3.min.js"></script>
-	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-	<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
-	<script src="assets/js/appear.js"></script>
-	<script src="assets/js/jquery.simple-text-rotator.min.js"></script>
-	<script src="assets/js/jqBootstrapValidation.js"></script>
-	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-	<script src="assets/js/gmaps.js"></script>
-	<script src="assets/js/isotope.pkgd.min.js"></script>
-	<script src="assets/js/imagesloaded.pkgd.js"></script>
-	<script src="assets/js/jquery.flexslider-min.js"></script>
-	<script src="assets/js/jquery.magnific-popup.min.js"></script>
-	<script src="assets/js/jquery.fitvids.js"></script>
-	<script src="assets/js/smoothscroll.js"></script>
-	<script src="assets/js/wow.min.js"></script>
-	<script src="assets/js/contact.js"></script>
-	<script src="assets/js/custom.js"></script>
+		<?php include('assets/php/includes/foot-scripts.html');?>
 
 </body>
 </html>
