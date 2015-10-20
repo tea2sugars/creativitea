0018dc2bc219a15007f78c894c443d5c80dfd293
Rik Barwick <rik@creativitea.co.uk>
2015-10-20 12:10:24 +0100

    HTML Sitemap

---
+++ Site/sitemap.php
@@ -0,0 +1,146 @@
+<?php include('perch/runtime.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Site Map">
+	<meta name="author" content="">
+	
+	<title>Creativitea - The brand, digital and print design agency | London, Guildford, Suffolk</title>
+	
+		<?php include('assets/php/includes/head-css.html');?>
+
+</head>
+<body>
+
+
+<?php include('assets/php/includes/nav-fixed.html');?>	
+
+
+	<!-- WRAPPER -->
+	<div class="wrapper">
+
+		<!-- CONTACT -->
+		<section class="module-small">
+
+			<div class="container">
+
+				<!-- MODULE TITLE -->
+				<div class="row">
+
+					<div class="col-sm-8 col-sm-offset-2">
+
+						<h1>Find your way round our website</h1>
+						
+						<ul>
+							<li>
+								<h4><a href="/">Homepage</a></h4>
+								<p>How we are and what we do.</p>
+							</li>
+							<li>
+								<h4>About Us</h4>
+								<p>More detail about us and our design agency.</p>
+								<ul>
+									<li>
+										<h5><a href="story">Our Story</a></h5>
+										<p>Who we are and why we do what we do</p>
+									</li>
+									<li>
+										<h5><a href="team">The Team</a></h5>
+										<p>Individuals making up a great team.</p>
+									</li>
+									<li>
+										<h5><a href="creative-process">Creative Process</a></h5>
+										<p>Not all process are the same, not all are equal. Here's ours.</p>
+									</li>
+									<li>
+										<h5><a href="clients">Our Wonderful Clients</a></h5>
+										<p>We have had the pleasure of working with some great brands, organisations and people from all over the world.</p>
+									</li>
+									<li>
+										<h5><a href="blog/archive.php?cat=press">Press</a></h5>
+										<p>Articles about our design agency.</p>
+									</li>
+									<li>
+										<h5><a href="blog/archive.php?cat=awards">Awards</a></h5>
+										<p>We've won a few design awards.</p>
+									</li>
+									<li>
+										<h5><a href="blog/why-we-joined-the-dba">The Design Business Association</a></h5>
+										<p>Why we joined the DBA.</p>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<h4>Design Services</h4>
+								<p>Services we can help with</p>
+								<ul>
+									<li>
+										<h5><a href="branding">Branding</a></h5>
+										<p>The who, what and why of your business.</p>
+									</li>
+									<li>
+										<h5><a href="digital-design">Digital Design</a></h5>
+										<p>Delivering your brand message and your product marketing instantly anywhere in the world.</p>
+									</li>
+									<li>
+										<h5><a href="print-design">Print Design</a></h5>
+										<p>Not all process are the same, not all are equal. Here's ours.</p>
+									</li>
+									<li>
+										<h5><a href="video-production">Video Production</a></h5>
+										<p>Beyond the still.</p>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<h4><a href="portfolio">Portfolio</a></h4>
+								<p>Our Creative Design Work.</p>
+							</li>
+							<li>
+								<h4><a href="blog">Blog</a></h4>
+								<p>The latest news and musings from our agency, the industry and life in general.</p>
+							</li>
+							<li>
+								<h4><a href="contact">Contact Us</a></h4>
+								<p>Contact Our Team.</p>
+							</li>
+						</ul>
+							
+						
+					</div>
+					
+					
+		</section>
+		<!-- /CONTACT -->
+
+		<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+
+		<?php include('assets/php/includes/footer-services.html');?>
+		<?php include('assets/php/includes/footer-contact.html');?>
+	
+	<!-- Javascript files -->
+	<script src="assets/js/jquery-2.1.3.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
+	<script src="assets/js/appear.js"></script>
+	<script src="assets/js/jquery.simple-text-rotator.min.js"></script>
+	<script src="assets/js/jqBootstrapValidation.js"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="assets/js/gmaps.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<script src="assets/js/imagesloaded.pkgd.js"></script>
+	<script src="assets/js/jquery.flexslider-min.js"></script>
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/jquery.fitvids.js"></script>
+	<script src="assets/js/smoothscroll.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/contact.js"></script>
+	<script src="assets/js/custom.js"></script>
+	<script type="text/javascript" src="https://s3-eu-west-1.amazonaws.com/share.typeform.com/embed.js"></script>
+
+</body>
+</html>
