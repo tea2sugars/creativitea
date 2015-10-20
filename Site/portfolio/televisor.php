e0a9b9ba20db541c400eb24a703540dec15a0a29
Rik Barwick <rik@creativitea.co.uk>
2015-10-20 11:17:35 +0100

    fixed layout

--- Site/portfolio/televisor.php
+++ Site/portfolio/televisor.php
@@ -82,7 +82,7 @@
 
 				<div class="row">
 
-					<div class="col-md-8">
+					<div class="col-md-8 col-sm-offset-4" >
 
 						<div class="row">
 							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
