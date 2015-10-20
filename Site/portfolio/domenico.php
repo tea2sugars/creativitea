f2bbf074ab9357d6bf8c3dbcba4437b2f76afec8
Rik Barwick <rik@creativitea.co.uk>
2015-10-20 11:34:42 +0100

    made subheader white brightened the domenico header image

--- Site/portfolio/domenico.php
+++ Site/portfolio/domenico.php
@@ -16,7 +16,7 @@
 
 
 	<!-- HOME -->
-	<section class="module module-header bg-dark bg-dark-70" data-background="../assets/images/projects/Domenico_general_header.jpg">
+	<section class="module module-header bg-dark bg-dark-50" data-background="../assets/images/projects/Domenico_general_header.jpg">
 
 			<div class="container">
 
