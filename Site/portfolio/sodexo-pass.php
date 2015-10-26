7aa6013b553d14d6e36c48f2303f2b2f3bd85078
Rik Barwick <rik@creativitea.co.uk>
2015-10-26 16:51:22 +0000

    copy update

--- Site/portfolio/sodexo-pass.php
+++ Site/portfolio/sodexo-pass.php
@@ -86,22 +86,25 @@
 
 							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
 								<h5 class="m-t-0">History</h5>
-								<p>Sodexo is the worldwide leader in Quality of Life services. Sodexo develops, manages and delivers a diverse range of services, designed to improve the quality of life for their clients and customers.
+								<p>Sodexo is the worldwide leader in quality of life services. Sodexo develops, manages and delivers a diverse range of services, designed to improve the quality of life for their clients and customers.
 </p>
 							</div>
 
 							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
 								<h5 class="m-t-0">Our Brief</h5>
-								<p>Sodexo approached us to redesign the artwork for one of their Benefit and Reward services; multi-retailer gift voucher SayShopping Pass. The current design had been used for a long time and with Sodexo itself going through a identity change and update it was felt that the vouchers needed to reflect this and be inline with the modern look of the Sodexo brand as a whole. There was a requirement for three variants of the artwork to be designed for the different monitory values, and that security printing needed to be considered to prevent forgery or counterfeiting of the vouchers.
+								<p>Sodexo approached us to redesign the artwork for one of their Benefit and Reward services; multi-retailer gift voucher SayShopping Pass. The current design had been used for a long time and with Sodexo itself going through a identity change and update, it was felt that the vouchers needed to reflect this and be inline with the modern look of the Sodexo brand as a whole. There was a requirement for three variants of the artwork to be designed for the different monitory values, and that security printing needed to be considered to prevent forgery or counterfeiting of the vouchers.
 								</p>
 							</div>
 														
 							<div class="col-sm-12 col-md-12 col-lg-12 m-b-30">
 								<h5 class="m-t-0">What we did</h5>
 								<p>The first steps we took was making all artworks designed a consistent collection of artworks, making for better brand recognition, as the existing artworks took the direction of different designs making it hard to understand that they where the same voucher series. We also approached the concept that the identity of the vouchers should also be recognisable, a sub-brand that fitted well into the existing portfolio of Sodexo’s products and services.</p>
-								<p>After presenting several concept designs to the team, we began to develop a new art direction for the vouchers artwork to head in. The final series of artworks uses colours form the brands palette in bright and distinguished combinations; the use of ‘micro-line’ patterns in a bespoke designed watermark and a new illustrated pattern representing the wide choice of gifts you could use the voucher for.</p>
-								<p>Working closely with the production team in place, we implemented the use of holographic, gold foil areas and colour changing thermo inks on both sides of the voucher into the artworks design, to increase the security and officiation of the vouchers and to prevent the risk of them being easily replicated.</p>
-								<p>The final vouchers are now proudly part of the Sodexo product portfolio, bringing a brighter and modern look and feel and giving a long serving product a new lease of life.</p>
+
+<p>After presenting several concept designs to the team, we began to develop a new art direction for the vouchers artwork to head in. The final series of artworks uses colours form the brands palette in bright and distinguished combinations; the use of &lsquo;mico-line&rsquo; patterns in a bespoke designed watermark and a new illustrated pattern representing the wide choice of gifts you could use the voucher for.</p>
+
+<p>Working closely with the production team in place, we implemented the use of holographic, gold foil areas and colour changing thermo inks on both sides of the voucher into the artworks design, to increase the security and officiation of the vouchers and to prevent the risk of them being easily replicated.</p>
+
+<p>The final vouchers are now proudly part of the Sodexo product portfolio, bringing a brighter and modern look and feel and giving a long serving product a new lease of life.</p>
 
 								<?php include('../assets/php/includes/portfolio-cta.html');?>
 							</div>
