5590288c709adc90f3e3045506c4caaa309f066e
Rik Barwick <rik@creativitea.co.uk>
2015-11-11 14:48:13 +0000

    blog template tweak

--- Site/blog/index.php
+++ Site/blog/index.php
@@ -92,11 +92,6 @@ window._fbq.push(['track', '6038156343123', {'value':'0.01','currency':'GBP'}]);
 				
 				<div class="row multi-columns-row post-columns">
 					
-							<!-- this is an example blog homepage showing a simple call to perch_blog_recent_posts()
-			
-			Posts are displayed using the templates stored in perch/apps/perch_blog/templates/blog you can edit these as you wish, making sure that the 
-			paths used in these templates are correct for your installation.
-			 -->
 			 		<?php 
 				 		perch_blog_recent_posts(12);
 					?>
