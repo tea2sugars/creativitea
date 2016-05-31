0c27b53555bc3e5663b7b3eafc350f18e26b5b47
Rik Barwick <rik@creativitea.co.uk>
2016-05-31 22:38:35 +0100

    Website Design Surrey page

---
+++ Site/website-design-surrey.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Creativitea offers Website Design services including UX Design, Website Development, Email Marketing and SEO from their studio in Surrey.">
+	<meta name="keywords" content="website design surrey, web design surrey, website design sutton, web design sutton"
+	<meta name="author" content="">
+	
+	<title>Creativitea - A Website Design Agency in Surrey | Web Development</title>
+	
+		<?php include('assets/php/includes/head-css.html');?>
+
+	
+	<!--Share widget-->
+	<script type="text/javascript">var switchTo5x=true;</script>
+	<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+	<script type="text/javascript">stLight.options({publisher: "0750579a-dfe6-4d2b-a96d-da69d0aa188f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+	
+</head>
+<body>
+
+	<?php include('assets/php/includes/nav-fixed.html');?>
+
+
+	<!-- WRAPPER -->
+	<div class="wrapper">
+
+		
+
+		<!-- BLOG SINGLE -->
+		<section class="module">
+
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-sm-8 col-sm-offset-2">
+						<h1>Website Design Surrey</h1>
+						<!-- POST -->
+						<div class="post">
+
+							<article class="post-entry">
+
+								<p class="lead">Creativitea have provided clients with website design in Surrey for 8 years now and continue to work with local businesses to build their brand through their digital presence.</p>
+
+								<p>Surrey offers a huge opportunity for businesses because of the transport links to London and the South coast so it sees a lot of good business networking opportunities. It's an affluent area being so close to London so most of our industry experience in Surrey is in the Luxury or Professional Market.</p>
+								
+								<p>The gaming industry has also served us well in the past with Guildford once being known as the gaming capital of Britain. We've worked for Electronic Arts, Trion Worlds, Activision and Atari who have all been based here.</p>
+								
+								<h3>Our local experience</h3>
+								
+								<p>We've designed websites for Surrey based clients such as Tante Marie Culinary Academy and DJST.</p>
+								
+								<p>We are also proud to have supported Surrey based charities with their website design projects. These include Communilab and Surrey Youth Focus.</p>
+								<h3>Our studio</h3>
+								<p>In 2016 we said goodbye to our studio in Guildford and moved to Sutton, Surrey. This offers us a more direct route to the tech hub of Shoreditch. Also being closer to London offers us more networking opportunities which helps broaden our knowledge and horizons.</p>
+								<h4>Studio Address</h4>
+								<p>Unfortunately due to the privacy of some of our work visitors to our studio is not allowed. However if you would like to send us something (we like presents) our address is;</p>
+								<div itemscope itemtype="http://schema.org/LocalBusiness">
+									<p>
+										<span itemprop="name">Creativitea Ltd</span>
+										<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+									    	<span itemprop="streetAddress">The Cabin, 36 Welbeck Road</span>
+											<span itemprop="addressLocality">Sutton</span>,
+											<span itemprop="addressRegion">Surrey</span>
+											<span itemprop="postalCode">SM1 3NW</span>
+								   		</div>
+									</p>
+								</div>
+							</article>
+						</div>
+								
+								<p class="yellow lead">&ndash;</p>
+
+								<p>Our focus is still in supplying website design services to Surrey so if you have a project that we could help you with please <a href="contact" title="contact us">contact us</a>.</p>
+							</article>
+							
+						</div>
+						<!-- /POST -->
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</section>
+		<!-- /BLOG SINGLE -->
+		
+		<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+
+		<?php include('assets/php/includes/footer-services.html');?>
+		<?php include('assets/php/includes/footer-contact.html');?>
+		<?php include('assets/php/includes/foot-scripts.html');?>
+
+</body>
+</html>
