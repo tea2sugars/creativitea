90d4944eb3a2842b70bf24e066749dff99e65f65
Rik Barwick <rik@creativitea.co.uk>
2016-06-01 15:04:59 +0100

    brand design surrey page

---
+++ Site/brand-design-surrey.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Creativitea offers Brand Design services including Brand Strategy, Visual Identity and Brand Management from their studio in Surrey.">
+	<meta name="keywords" content="brand design surrey, branding surrey, brand design sutton, branding sutton"
+	<meta name="author" content="">
+	
+	<title>Creativitea - A Brand Design Agency in Surrey | Branding and Visual Identity</title>
+	
+		<?php include('assets/php/includes/head-css.html');?>
+
+	
+	<!--Share widget-->
+	<script type="text/javascript">var switchTo5x=true;</script>
+	<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+	<script type="text/javascript">stLight.options({publisher: "0750579a-dfe6-4d2b-a96d-da69d0aa188f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+	
+</head>
+<body>
+
+	<?php include('assets/php/includes/nav-fixed.html');?>
+
+
+	<!-- WRAPPER -->
+	<div class="wrapper">
+
+		
+
+		<!-- BLOG SINGLE -->
+		<section class="module">
+
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-sm-8 col-sm-offset-2">
+						<h1>Brand Design Surrey</h1>
+						<!-- POST -->
+						<div class="post">
+
+							<article class="post-entry">
+
+								<p class="lead">At Creativitea we have provided Surrey based clients with brand design for the past 8 years.</p>
+
+								<p>Since starting the agency in 2008 we have been providing clients with brand design in Surrey, working with national and local businesses to connect with their customers.</p>
+								
+								
+								<h3>Local experience</h3>
+								
+								<p>We’ve worked with organisations from all different sectors with effective, thoughtful brand designs. Our projects included the works from the gaming industry with Electronic Arts, Trion Worlds, Activision and Atari, to luxury brands such as Marchon Eyewear and Nike, to smaller companies such as Tante Marie and Corsaire.
+
+								<p>We are also proud to have supported Surrey based charities including Surrey Youth Focus and Communilab, with their brand design projects.</p>
+								<h3>The studio</h3>
+								<p>2016, saw us move our studio from Guildford to Sutton. This offers a more direct route to London whilst still having a presence in Surrey, our original home. With direct transport links into central London and the wider areas of Surrey, we are able to continue to provide our branding services to both areas.</p>
+								
+								<p>Unfortunately due to the privacy of some of our clients work we're under contract to not allow visitors to the studio. However if you would like to send us something (we all like presents) our address is;</p>
+								<div itemscope itemtype="http://schema.org/LocalBusiness">
+										<span itemprop="name">Creativitea Ltd</span>
+										<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+									    	<span itemprop="streetAddress">The Cabin, 36 Welbeck Road</span>
+											<span itemprop="addressLocality">Sutton</span>,
+											<span itemprop="addressRegion">Surrey</span>
+											<span itemprop="postalCode">SM1 3NW</span>
+								   		</div>
+								</div>
+								<p class="yellow lead">&ndash;</p>
+
+								<p>Our focus is still in supplying brand design services to Surrey so if you have a project that we could help you with please <a href="contact" title="contact us">contact us</a> and we'll visit you or meet for a coffee and a chat.</p>
+							</article>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</section>
+		<!-- /BLOG SINGLE -->
+		
+		<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+
+		<?php include('assets/php/includes/footer-services.html');?>
+		<?php include('assets/php/includes/footer-contact.html');?>
+		<?php include('assets/php/includes/foot-scripts.html');?>
+
+</body>
+</html>
