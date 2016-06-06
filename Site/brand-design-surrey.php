0abdbb23fb1cb70e14dbea184b0a470b19eb5169
Rik Barwick <rik@creativitea.co.uk>
2016-06-06 10:50:20 +0100

    updated content on localised branding page

--- Site/brand-design-surrey.php
+++ Site/brand-design-surrey.php
@@ -52,6 +52,30 @@
 								<p>We’ve worked with organisations from all different sectors with effective, thoughtful brand designs. Our projects included the works from the gaming industry with Electronic Arts, Trion Worlds, Activision and Atari, to luxury brands such as Marchon Eyewear and Nike, to smaller companies such as Tante Marie and Corsaire.
 
 								<p>We are also proud to have supported Surrey based charities including Surrey Youth Focus and Communilab, with their brand design projects.</p>
+								
+								<h4>Brand Design Solutions</h4>
+								<p>Our award-winning brand team pride themselves on being able to develop clients brands through creative thought, strategy and design execution. Discover more about our digital services below;</p>
+								<p>
+									<ul>
+										<li class="list-indent">
+											<h5><a href="brand-stylists" title="Brand Stylists">
+												Brand Stylists
+											</a></h5>
+											<p>Working with your business to design your logo, colour palette, tone of voice and visual identity.</p>
+											
+										</li>
+										<!--<li class="list-indent">
+											<h5><a href="digital-marketing" title="Digital Marketing">Digital Marketing</a></h5>
+											<p>Helping you drive traffic, drive sales and convert your users into leads</p>
+										</li>-->
+										<li class="list-indent">
+											<h5><a href="brand-management" title="Brand Management">Brand Management</a></h5>
+											<p>Ensuring your business communications stay on-brand.</p>
+										</li>
+									</ul>
+								</p>
+
+
 								<h3>The studio</h3>
 								<p>2016, saw us move our studio from Guildford to Sutton. This offers a more direct route to London whilst still having a presence in Surrey, our original home. With direct transport links into central London and the wider areas of Surrey, we are able to continue to provide our branding services to both areas.</p>
 								
@@ -74,11 +98,109 @@
 					</div>
 
 				</div>
+				
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-2">
+						<p>Whether it’s a new brand design, identity or an existing one in need of a refresh, we have the experience and expertise to help develop a striking brand identity that people want buy into and be part of.</p>
+							<a href="contact" class="btn btn-d btn-round">Contact Us</a> <a href="portfolio" class="btn btn-g btn-round">View work</a>
+					</div>
+							
+						<!-- /POST -->
+
+					</div>
+				</div>
+				
+				
 
 			</div>
 
 		</section>
 		<!-- /BLOG SINGLE -->
+		
+				<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+		
+		<!-- PORTFOLIO -->
+		<section class="module-small p-t-20 p-b-40 p-t-sm-0" id="portfolio">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="align-center">Some of our Brand Design Projects</h2>
+				</div>
+				<div class="row">
+					<!-- PORTFOLIO LIST -->
+			<ul id="works-grid" class="works-grid works-grid-4 works-hover-w">
+				
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item branding print branding digital">
+					<a href="./portfolio/domenico">
+						<div class="work-image">
+							<img src="assets/images/projects/Domenico_portfolio_thumbnail.jpg" alt="Domenico Pizza Branding"/>
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Domenico Pizza</h3>
+							<div class="work-descr ">
+								Hand-Crafted Branding &amp; Digital Design
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+				
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item print branding">
+					<a href="./portfolio/gentspack">
+						<div class="work-image">
+							<img src="assets/images/projects/Gents_thumbnail.jpg" alt="Gents Pack Rebrand and Packaging"/>
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Gents Pack</h3>
+							<div class="work-descr ">
+								Brand design and Packaging
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item branding">
+					<a href="./portfolio/river">
+						<div class="work-image">
+							<img src="assets/images/projects/River_thumbnail.jpg" alt="River Brand & Visual Identity"/>
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">River</h3>
+							<div class="work-descr ">
+								Brand & Visual Identity
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+				
+				<li class="work-item branding print disrupt digital">
+					<a href="./portfolio/powwownow">
+						<div class="work-image">
+							<img src="assets/images/projects/Pow_portfolio_thumbnail.jpg" alt="Powwownow Branding"/>
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Powwownow Branding</h3>
+							<div class="work-descr ">
+								Disruptive thinking for better brand communication
+							</div>
+						</div>
+					</a>
+				</li>
+				
+				
+							</ul>
+			<!-- /PORTFOLIO LIST -->
+				</div>
+			</div>
+			
+		</section>
+		<!-- /PORTFOLIO -->
 		
 		<!-- DIVIDER -->
 		<hr class="divider-w">
