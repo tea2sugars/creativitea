a80c05733146b88e24131e6d5c9e194c318ccab6
Rik Barwick <rik@creativitea.co.uk>
2016-06-06 10:57:23 +0100

    updated content on localised branding page

--- Site/brand-design-surrey.php
+++ Site/brand-design-surrey.php
@@ -1,214 +1,226 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
-		<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Creativitea offers Brand Design services including Brand Strategy, Visual Identity and Brand Management from their studio in Surrey.">
-	<meta name="keywords" content="brand design surrey, branding surrey, brand design sutton, branding sutton"
-	<meta name="author" content="">
-	
-	<title>Creativitea - A Brand Design Agency in Surrey | Branding and Visual Identity</title>
-	
-		<?php include('assets/php/includes/head-css.html');?>
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta content="Surrey based design studio Creativitea offers Brand Design Services including Logo Design Brand Strategy and Brand Management." name="description">
+	<meta content="brand design surrey, branding surrey, brand design sutton, branding sutton" name="keywords">
+	<meta content="" name="author">
 
-	
-	<!--Share widget-->
-	<script type="text/javascript">var switchTo5x=true;</script>
-	<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
-	<script type="text/javascript">stLight.options({publisher: "0750579a-dfe6-4d2b-a96d-da69d0aa188f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
-	
+	<title>Creativitea - A Brand Design Agency in Surrey | Branding and Visual Identity</title><?php include('assets/php/includes/head-css.html');?><!--Share widget-->
+
+	<script type="text/javascript">
+	var switchTo5x=true;
+	</script>
+	<script src="http://w.sharethis.com/button/buttons.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	stLight.options({publisher: "0750579a-dfe6-4d2b-a96d-da69d0aa188f", doNotHash: false, doNotCopy: false, hashAddressBar: false});
+	</script>
 </head>
+
 <body>
+	<?php include('assets/php/includes/nav-fixed.html');?><!-- WRAPPER -->
 
-	<?php include('assets/php/includes/nav-fixed.html');?>
 
-
-	<!-- WRAPPER -->
 	<div class="wrapper">
-
-		
-
 		<!-- BLOG SINGLE -->
+
+
 		<section class="module">
-
 			<div class="container">
-
 				<div class="row">
-
 					<div class="col-sm-8 col-sm-offset-2">
 						<h1>Brand Design Surrey</h1>
 						<!-- POST -->
+
+
 						<div class="post">
-
 							<article class="post-entry">
-
 								<p class="lead">At Creativitea we have provided Surrey based clients with brand design for the past 8 years.</p>
 
+
 								<p>Since starting the agency in 2008 we have been providing clients with brand design in Surrey, working with national and local businesses to connect with their customers.</p>
-								
-								
-								<h3>Local experience</h3>
-								
-								<p>We’ve worked with organisations from all different sectors with effective, thoughtful brand designs. Our projects included the works from the gaming industry with Electronic Arts, Trion Worlds, Activision and Atari, to luxury brands such as Marchon Eyewear and Nike, to smaller companies such as Tante Marie and Corsaire.
+
+
+								<h3>Local Experience</h3>
+
+
+								<p>We’ve worked with organisations from all different sectors with effective, thoughtful brand designs. Our projects included the works from the gaming industry with Electronic Arts, Trion Worlds, Activision and Atari, to luxury brands such as Marchon Eyewear and Nike, to smaller companies such as Tante Marie and Corsaire.</p>
+
 
 								<p>We are also proud to have supported Surrey based charities including Surrey Youth Focus and Communilab, with their brand design projects.</p>
-								
-								<h4>Brand Design Solutions</h4>
+
+
+								<h3>Brand Design Solutions</h3>
+
+
 								<p>Our award-winning brand team pride themselves on being able to develop clients brands through creative thought, strategy and design execution. Discover more about our digital services below;</p>
+
+
 								<p>
-									<ul>
-										<li class="list-indent">
-											<h5><a href="brand-stylists" title="Brand Stylists">
-												Brand Stylists
-											</a></h5>
-											<p>Working with your business to design your logo, colour palette, tone of voice and visual identity.</p>
-											
-										</li>
-										<!--<li class="list-indent">
-											<h5><a href="digital-marketing" title="Digital Marketing">Digital Marketing</a></h5>
-											<p>Helping you drive traffic, drive sales and convert your users into leads</p>
-										</li>-->
-										<li class="list-indent">
-											<h5><a href="brand-management" title="Brand Management">Brand Management</a></h5>
-											<p>Ensuring your business communications stay on-brand.</p>
-										</li>
-									</ul>
 								</p>
 
 
-								<h3>The studio</h3>
+								<ul>
+									<li class="list-indent">
+										<h5><a href="brand-stylists" title="Brand Stylists">Brand Stylists</a>
+										</h5>
+
+
+										<p>Working with your business to design your logo, colour palette, tone of voice and visual identity.</p>
+									</li>
+									<!--<li class="list-indent">
+                                            <h5><a href="digital-marketing" title="Digital Marketing">Digital Marketing</a></h5>
+                                            <p>Helping you drive traffic, drive sales and convert your users into leads</p>
+                                        </li>-->
+
+
+									<li class="list-indent">
+										<h5><a href="brand-management" title="Brand Management">Brand Management</a>
+										</h5>
+
+
+										<p>Ensuring your business communications stay on-brand.</p>
+									</li>
+								</ul>
+								<p></p>
+
+								<h3>The Studio</h3>
+
+
 								<p>2016, saw us move our studio from Guildford to Sutton. This offers a more direct route to London whilst still having a presence in Surrey, our original home. With direct transport links into central London and the wider areas of Surrey, we are able to continue to provide our branding services to both areas.</p>
-								
+
+
 								<p>Unfortunately due to the privacy of some of our clients work we're under contract to not allow visitors to the studio. However if you would like to send us something (we all like presents) our address is;</p>
+
+
 								<div itemscope itemtype="http://schema.org/LocalBusiness">
-										<span itemprop="name">Creativitea Ltd</span>
-										<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-									    	<span itemprop="streetAddress">The Cabin, 36 Welbeck Road</span>
-											<span itemprop="addressLocality">Sutton</span>,
-											<span itemprop="addressRegion">Surrey</span>
-											<span itemprop="postalCode">SM1 3NW</span>
-								   		</div>
+									<span itemprop="name">Creativitea Ltd</span>
+
+									<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+										<span itemprop="streetAddress">The Cabin, 36 Welbeck Road</span> <span itemprop="addressLocality">Sutton</span>, <span itemprop="addressRegion">Surrey</span> <span itemprop="postalCode">SM1 3NW</span>
+									</div>
 								</div>
+
+
 								<p class="yellow lead">&ndash;</p>
 
-								<p>Our focus is still in supplying brand design services to Surrey so if you have a project that we could help you with please <a href="contact" title="contact us">contact us</a> and we'll visit you or meet for a coffee and a chat.</p>
+
+								<p>Whether it’s a new brand design, identity or an existing one in need of a refresh, we have the experience and expertise to help develop a striking brand identity that people want buy into and be part of.</p>
+								<a class="btn btn-d btn-round" href="contact">Contact Us</a> <a class="btn btn-g btn-round" href="portfolio">View work</a>
 							</article>
 						</div>
-
-					</div>
-
-				</div>
-				
-				<div class="row">
-					<div class="col-sm-8 col-sm-offset-2">
-						<p>Whether it’s a new brand design, identity or an existing one in need of a refresh, we have the experience and expertise to help develop a striking brand identity that people want buy into and be part of.</p>
-							<a href="contact" class="btn btn-d btn-round">Contact Us</a> <a href="portfolio" class="btn btn-g btn-round">View work</a>
-					</div>
-							
-						<!-- /POST -->
-
 					</div>
 				</div>
-				
-				
-
 			</div>
-
 		</section>
 		<!-- /BLOG SINGLE -->
-		
-				<!-- DIVIDER -->
+		<!-- DIVIDER -->
+
 		<hr class="divider-w">
 		<!-- /DIVIDER -->
-		
 		<!-- PORTFOLIO -->
+
+
 		<section class="module-small p-t-20 p-b-40 p-t-sm-0" id="portfolio">
 			<div class="container-fluid">
 				<div class="row">
 					<h2 class="align-center">Some of our Brand Design Projects</h2>
 				</div>
+
+
 				<div class="row">
 					<!-- PORTFOLIO LIST -->
-			<ul id="works-grid" class="works-grid works-grid-4 works-hover-w">
-				
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item branding print branding digital">
-					<a href="./portfolio/domenico">
-						<div class="work-image">
-							<img src="assets/images/projects/Domenico_portfolio_thumbnail.jpg" alt="Domenico Pizza Branding"/>
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Domenico Pizza</h3>
-							<div class="work-descr ">
-								Hand-Crafted Branding &amp; Digital Design
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-				
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item print branding">
-					<a href="./portfolio/gentspack">
-						<div class="work-image">
-							<img src="assets/images/projects/Gents_thumbnail.jpg" alt="Gents Pack Rebrand and Packaging"/>
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Gents Pack</h3>
-							<div class="work-descr ">
-								Brand design and Packaging
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
 
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item branding">
-					<a href="./portfolio/river">
-						<div class="work-image">
-							<img src="assets/images/projects/River_thumbnail.jpg" alt="River Brand & Visual Identity"/>
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">River</h3>
-							<div class="work-descr ">
-								Brand & Visual Identity
+
+					<ul class="works-grid works-grid-4 works-hover-w" id="works-grid">
+						<!-- PORTFOLIO ITEM -->
+
+
+						<li class="work-item branding print branding digital">
+							<a href="./portfolio/domenico">
+							<div class="work-image"><img alt="Domenico Pizza Branding" src="assets/images/projects/Domenico_portfolio_thumbnail.jpg">
 							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-				
-				<li class="work-item branding print disrupt digital">
-					<a href="./portfolio/powwownow">
-						<div class="work-image">
-							<img src="assets/images/projects/Pow_portfolio_thumbnail.jpg" alt="Powwownow Branding"/>
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Powwownow Branding</h3>
-							<div class="work-descr ">
-								Disruptive thinking for better brand communication
+
+
+							<div class="work-caption">
+								<h3 class="work-title">Domenico Pizza</h3>
+
+
+								<div class="work-descr">
+									Branding &amp; Digital Design
+								</div>
+							</div></a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+						<!-- PORTFOLIO ITEM -->
+
+
+						<li class="work-item print branding">
+							<a href="./portfolio/gentspack">
+							<div class="work-image"><img alt="Gents Pack Rebrand and Packaging" src="assets/images/projects/Gents_thumbnail.jpg">
 							</div>
-						</div>
-					</a>
-				</li>
-				
-				
-							</ul>
-			<!-- /PORTFOLIO LIST -->
+
+
+							<div class="work-caption">
+								<h3 class="work-title">Gents Pack</h3>
+
+
+								<div class="work-descr">
+									Brand design and Packaging
+								</div>
+							</div></a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+						<!-- PORTFOLIO ITEM -->
+
+
+						<li class="work-item branding">
+							<a href="./portfolio/river">
+							<div class="work-image"><img alt="River Brand & Visual Identity" src="assets/images/projects/River_thumbnail.jpg">
+							</div>
+
+
+							<div class="work-caption">
+								<h3 class="work-title">River</h3>
+
+
+								<div class="work-descr">
+									Brand & Logo Design
+								</div>
+							</div></a>
+						</li>
+						<!-- /PORTFOLIO ITEM -->
+
+
+						<li class="work-item branding print disrupt digital">
+							<a href="./portfolio/powwownow">
+							<div class="work-image"><img alt="Powwownow Branding" src="assets/images/projects/Pow_portfolio_thumbnail.jpg">
+							</div>
+
+
+							<div class="work-caption">
+								<h3 class="work-title">Powwownow Branding</h3>
+
+
+								<div class="work-descr">
+									Brand Communication
+								</div>
+							</div></a>
+						</li>
+					</ul>
+					<!-- /PORTFOLIO LIST -->
 				</div>
 			</div>
-			
 		</section>
 		<!-- /PORTFOLIO -->
-		
 		<!-- DIVIDER -->
+
 		<hr class="divider-w">
 		<!-- /DIVIDER -->
-
-		<?php include('assets/php/includes/footer-services.html');?>
-		<?php include('assets/php/includes/footer-contact.html');?>
-		<?php include('assets/php/includes/foot-scripts.html');?>
-
+		<?php include('assets/php/includes/footer-services.html');?><?php include('assets/php/includes/footer-contact.html');?><?php include('assets/php/includes/foot-scripts.html');?>
+	</div>
 </body>
 </html>
